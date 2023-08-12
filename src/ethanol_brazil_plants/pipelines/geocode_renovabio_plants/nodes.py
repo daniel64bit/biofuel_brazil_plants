4b2699c269797a042c4586c28da39b72bd2d648c
Daniel <danielrod147@gmail.com>
2023-08-12 16:39:41 -0300

    Allowing the choice between Google or Bing for geocoding

--- src/ethanol_brazil_plants/pipelines/geocode_renovabio_plants/nodes.py
+++ src/ethanol_brazil_plants/pipelines/geocode_renovabio_plants/nodes.py
@@ -79,25 +79,36 @@ def selenium_geocode(
     gis: str,
 ) -> pd.DataFrame:
     """
-    Geocode addresses using Selenium and Bing or Google Maps
+    Geocode addresses using Selenium and Bing or Google Maps.
+    Args:
+        mozilla_service: Selenium service for Firefox
+        mozilla_options: Selenium options for Firefox
+        address_list_v1: List of addresses to be geocoded
+        address_list_v2: List of addresses to be geocoded in case version 1 fails
+        first_iter_sleep_time: Sleep time for first iteration
+        sleep_time: Sleep time for subsequent iterations
+        gis: GIS (Maps) to be used. Choose between 'bing' or 'google'
     """
     # Launch browser
     browser = webdriver.Firefox(service=mozilla_service, options=mozilla_options)
 
-    # Define GIS
+    # Define GIS URL
     if gis == 'bing':
         gis_url = 'https://www.bing.com/maps?q={}'
+        not_found_text = 'Não há resultados para:'  # PT-BR
     elif gis == 'google':
-        gis_url = 'https://www.google.com/maps?q={}'
+        gis_url = 'https://www.google.com.br/maps/search/{}'
+        not_found_text = 'O Google Maps não encontrou'  # PT-BR
     else:
         raise ValueError('Invalid GIS. Please choose between "bing" or "google".')
+
     # Geocoding addresses
     latitude = []
     longitude = []
 
     first_iter = True
     for i in range(len(address_list_v1)):
-        browser.get(f'https://www.bing.com/maps?q={address_list_v1[i]}')
+        browser.get(gis_url.format(address_list_v1[i]))
 
         # Longer sleep time for first iteration (browser opens)
         if first_iter is True:
@@ -109,9 +120,9 @@ def selenium_geocode(
         lat, long = get_latlong_from_url(browser.current_url)
 
         # If address is not found, try to geocode only the last 3 address columns
-        ckeck_results = re.findall('Não há resultados para:', browser.page_source)
+        ckeck_results = re.findall(not_found_text, browser.page_source)
         if len(ckeck_results) > 0:
-            browser.get(f'https://www.bing.com/maps?q={address_list_v2[i]}')
+            browser.get(gis_url.format(address_list_v2[i]))
             time.sleep(sleep_time)
 
             lat, long = get_latlong_from_url(browser.current_url)
@@ -132,6 +143,7 @@ def geocode_renovabio_plants(
     log_path: str,
     first_iter_sleep_time: float,
     sleep_time: float,
+    gis: str,
 ) -> pd.DataFrame:
     """
     Geocodes addresses using Selenium and Firefox
@@ -144,7 +156,8 @@ def geocode_renovabio_plants(
     df_latlong = selenium_geocode(
         mozilla_service, mozilla_options,
         address_list_v1, address_list_v2,
-        first_iter_sleep_time, sleep_time
+        first_iter_sleep_time, sleep_time,
+        gis
     )
 
     rf_renovabio_plants_geocoded = pd.concat([
