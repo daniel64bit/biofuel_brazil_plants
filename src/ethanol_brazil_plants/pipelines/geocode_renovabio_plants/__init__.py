59a4b8bc1efa6726241aa7cad594d740a3ff6355
Daniel <danielrod147@gmail.com>
2023-08-12 16:14:55 -0300

    pipeline function renamed

--- src/ethanol_brazil_plants/pipelines/geocode_renovabio_plants/__init__.py
+++ src/ethanol_brazil_plants/pipelines/geocode_renovabio_plants/__init__.py
@@ -3,8 +3,8 @@ This is a boilerplate pipeline 'geocode_renovabio_plants'
 generated using Kedro 0.18.12
 """
 
-from .pipeline import geocode_renovabio_plants
+from .pipeline import geocode_renovabio_plants_pipe
 
-__all__ = ["geocode_renovabio_plants"]
+__all__ = ["geocode_renovabio_plants_pipe"]
 
 __version__ = "0.1"
